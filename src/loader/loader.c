d441191742a21fd56cdac0e951ddef737eb45266
Just a man <vprovalov@yandex.ru>
2017-01-19 17:35:39 +0300

    upd loader. not tested.

--- src/loader/loader.c
+++ src/loader/loader.c
@@ -6,84 +6,94 @@
 #include <stdio.h>
 
 
-#define HANDLE_ERR( action ) \
-do { if( !(action) ) return ERR_IN_READING_FUNCTION; } while(0)
+#define TRY_READ( action, err ) \
+do { if( !(action) ) return err; } while(0)
 
-#define THROW_ERR( action ) \
+#define TRY_EXEC( action ) \
 do { size_t err = (action); if( err ) return err; } while(0)
+
+#define TRY_WITH_FINALLY( action, finally ) \
+do { size_t err = (action); if( err ) { (finally); return err; } } while(0)
+
 
 struct file_header {
 	uint64_t tag;
 	uint64_t version;
 
 	uint64_t str_pool_size;
-	uint64_t str_pool_offset;
-
-	uint64_t val_pool_size;
-	uint64_t val_pool_offset;
-
 	uint64_t functions_count;
-	uint64_t functions_offset;
 };
 
 
 static load_err_code_t
 load_str_const_pool( struct str_pool* pool, FILE* src ) {
-	char* chars = malloc( sizeof(char) * pool-> size );	
-	HANDLE_ERR( fread(chars, sizeof(char), pool-> size, src) );
+	TRY_READ( 
+		fread(pool-> char_at, sizeof(char), pool-> size, src),
+		ERR_IN_READING_CONST_POOL
+		);
 
-	split_by_lines( chars, pool-> by_idx ); //unimplimented
+	split_by_lines( pool-> char_at, pool-> str_at ); //unimplimented
 
-	return READ_OK;
-}
-
-static load_err_code_t
-load_val_const_pool( struct val_pool* pool, FILE* src ) {
-	pool-> by_idx = malloc( sizeof(uint64_t) * pool-> size );
-	HANDLE_ERR( fread(pool-> by_idx, sizeof(uint64_t), pool-> size, src) );
-
-	return READ_OK;
+	return LOAD_OK;
 }
 
 static inline load_err_code_t
 check_header( struct file_header header ) {
-	return  header.tag != 0xDEADDEADDEADDEAD || header.version != 1 ? ERR_INCORRENCT_HEADER : READ_OK;
+	return  header.tag != 0xDEADDEADDEADDEAD || header.version != 1 ? ERR_INCORRENCT_HEADER : LOAD_OK;
 }
 
 static load_err_code_t
 load_function( function_t* new_func, FILE* src ) {
 
-	HANDLE_ERR( fread( new_func, sizeof(uint64_t), 3, src ) );
+	TRY_READ( 
+		fread( new_func, sizeof(uint64_t), 3, src ),
+		ERR_IN_READING_FUNCTION_META
+		);
 	
-	HANDLE_ERR( fread( 
-		new_func.cmds = malloc( sizeof(char) * new_func-> cmds_count ),  
+	TRY_READ( fread( 
+		new_func-> cmds = malloc( sizeof(char) * new_func-> cmds_count ),  
 		sizeof(char), 
 		new_func-> cmds_count, 
-		src ) );
+		src ),
+		ERR_IN_READING_FUNCTION
+		);
 
-	return READ_OK;
+	return LOAD_OK;
 }
 
 static load_err_code_t
-load_functions( function_t** functions, FILE* src ) { 
-	uint64_t funcs_count;
-	HANDLE_ERR( fread( &funcs_count, sizeof(struct funcs_header), 1, src ) );
-	functions = malloc( sizeof(function_t*) * funcs_count );
+load_functions( function_t* functions, uint64_t const functions_count, FILE* src ) { 
+	for( size_t i = 0; i < funcs_count; i++ ) 
+		TRY_EXEC( load_function(function + i, src) );	
 
-	for( int i = 0; i < funcs_count; i++ ) 
-		THROW_ERR( load_function(function[i], src) );	
-
-	return READ_OK;
+	return LOAD_OK;
 }
+
 
 load_err_code_t
 load_src_file( vm_t* vm, FILE* src ) {
 	struct file_header header = {};
-	THROW_ERR( fread(&header, sizeof(struct file_header), 1, src) );
+	TRY_EXEC( fread(&header, sizeof(struct file_header), 1, src) );
 
-	load_functions( &(vm-> functions), src );
-	//TODO
-	return READ_OK;
+	vm_init_str_const_pool( &(vm-> const_str_pool), header-> str_pool_size );
+
+	TRY_WITH_FINALLY( 
+		load_str_const_pool( &(vm-> const_str_pool), src ),
+		vm_free_const_pool( &(vm-> const_str_pool), src )
+		);
+
+	vm-> functions = malloc( sizeof(function_t) * header-> functions_count );
+
+	TRY_WITH_FINALLY( 
+		load_functions( vm-> functions, vm-> funcs_count, src ),
+		{ 
+		vm_free_const_pool( &(vm-> const_str_pool) ); 
+		vm_free_functions( vm ); 
+		}
+		);
+
+	return LOAD_OK;
 }
+
 
 
