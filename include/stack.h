26532fabccf3771f2195b5d8e979f908cf7ea42b
Just a man <vprovalov@yandex.ru>
2017-01-19 18:20:20 +0300

    tested version

--- include/stack.h
+++ include/stack.h
@@ -3,18 +3,20 @@
 
 #include <stdlib.h>
 #include <stdint.h>
-#include "errors.h"
+#include "vm_errors.h"
 
 #define ERR_TYPE enum vm_err_code
 
 typedef struct stack {
 	size_t size, sp;
-	uint64_t* const data;
+	uint64_t* data;
 } stack_t;
 
-int stack_is_full( stack_t const stack );
+stack_t* stack_new( size_t const size );
 
-int stack_is_empty( stack_t const stack );
+int stack_is_full( stack_t const * const stack );
+
+int stack_is_empty( stack_t const * const stack );
 
 ERR_TYPE stack_push( stack_t* const stack, uint64_t const value );
 
@@ -26,6 +28,5 @@ ERR_TYPE stack_peek( stack_t* const stack, uint64_t * const var );
 void stack_free_data( stack_t* const stack );
 
 void stack_clear( stack_t* const stack );
-
 
 #endif
--- src/stack.c
+++ src/stack.c
@@ -4,9 +4,20 @@
 
 #define ERR_TYPE enum vm_err_code
 
-int stack_is_full( stack_t const stack ) { return stack.sp == stack.size; }
-`
-to_header int stack_is_empty( stack_t const stack ) { return stack.sp == 0; }
+stack_t* stack_new( size_t const size ) {
+	stack_t* new_stack = malloc( sizeof(stack_t) );		
+
+	*new_stack = (stack_t) {
+		.size = size, .sp = 0,
+		.data = malloc( sizeof(uint64_t) * size )
+	};
+
+	return new_stack;
+}
+
+int stack_is_full( stack_t const * const stack ) { return stack-> sp == stack-> size; }
+
+int stack_is_empty( stack_t const * const stack ) { return stack-> sp == 0; }
 
 ERR_TYPE stack_push( stack_t* const stack, uint64_t const value ) {
 	if( stack_is_full(stack) ) return STACK_OF;
@@ -16,7 +27,7 @@ ERR_TYPE stack_push( stack_t* const stack, uint64_t const value ) {
 }
 
 ERR_TYPE stack_peek( stack_t* const stack, uint64_t * const var ) {
-	if( stack_is_empty(*stack) ) return STACK_IS_EMPTY;
+	if( stack_is_empty(stack) ) return STACK_IS_EMPTY;
 	*var = stack-> data[ stack-> sp - 1 ];
 
 	return OK;
@@ -37,7 +48,17 @@ void stack_clear( stack_t* const stack ) { stack-> sp = 0; }
 
 #ifdef STACK_TEST
 
-int main( int argc, char** argv ) { }
+int main( int argc, char** argv ) { 
+	stack_t *new_stack = stack_new( 30 ); 
+	uint64_t value;
+
+	stack_push( new_stack, 34 );
+	stack_push( new_stack, 16 );
+	stack_peek( new_stack, &value );
+	stack_pop( new_stack, &value );
+
+	return value;
+}
 
 #endif
 
