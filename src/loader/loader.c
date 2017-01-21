97b7bef7f2e80b5312d0983d15d441e484f64897
Just a man <vprovalov@yandex.ru>
2017-01-21 03:37:51 +0300

    loader.c debugged

--- src/loader/loader.c
+++ src/loader/loader.c
@@ -2,9 +2,8 @@
 #include "function.h"
 #include "vm.h"
 #include "util.h"
-#include "debug.h"
-
 #include <stdio.h>
+#include "commands.h"
 
 
 #define TRY_READ( action, err ) \
@@ -26,12 +25,46 @@ struct file_header {
 
 
 #ifdef DEBUG
+	
+#define GET_MNEMONICS( id, cmd_name ) #cmd_name , 
+
+static char* bc_mnemonics[256] = {
+	FOR_BYTECODES(GET_MNEMONICS)
+	0
+};
+
+void print_function( function_t const * const func ) {
+	puts("\n\n__function_metadata__");	
+	printf( "+-- Name id: %x \n", func-> name_id );
+	printf( "+-- Args count: %u \n", func-> args_count );
+	printf( "+-- Cmds count: %u \n", func-> cmds_count );
+
+	puts("__bytecode_start__");
+	puts(" addr | bytecode | mnemonic ");
+
+	for( size_t i = 0; i < func-> cmds_count; i++ ) {
+		printf(" %04i : %08i", i, func-> cmds[i] );
+		printf(" : %s", bc_mnemonics[ func-> cmds[i] ] );
+		if( func-> cmds[i] == 0x01) {
+			printf(" 0x%08x\n", *((uint64_t*)(func-> cmds + i)) );
+			i+=8;
+		} else puts("");
+	}
+
+	puts("__bytecode_end__");
+}
 
 void print_file_header( struct file_header header ) {
 	printf("Tag: %u \n", header.tag );
 	printf("Version: %u \n", header.version );
 	printf("Pool size: %u \n", header.str_pool_size );
 	printf("Funcs count: %u \n", header.functions_count);
+}
+
+void print_str_pool( char** pool, size_t const count ) {
+	puts("___string_pool_start___");
+	for( size_t i = 0; i < count; i++ ) puts(pool[i]);
+	puts("___string_pool_end___");
 }
 
 #endif
@@ -45,15 +78,17 @@ load_str_const_pool( struct str_pool* pool, FILE* src ) {
 		ERR_IN_READING_CONST_POOL
 		);
 
-	split_by_lines( pool-> char_at, pool-> str_at );
+	split_by_lines( pool );
+
+	#ifdef DEBUG 
+	printf( "%u \n", pool-> str_count );
+	print_str_pool( pool-> str_at, pool-> str_count );
+	#endif
 
 	return LOAD_OK;
 }
 
-static inline load_err_code_t
-check_header( struct file_header header ) {
-	return  header.tag != 0xDEADDEADDEADDEAD || header.version != 1 ? ERR_INCORRECT_HEADER : LOAD_OK;
-}
+
 
 static load_err_code_t
 load_function( function_t* new_func, FILE* src ) {
@@ -61,7 +96,6 @@ load_function( function_t* new_func, FILE* src ) {
 		fread( new_func, sizeof(uint64_t), 3, src ),
 		ERR_IN_READING_FUNCTION_META
 		);
-
 	
 	TRY_READ( fread( 
 		new_func-> cmds = malloc( sizeof(char) * new_func-> cmds_count ),  
@@ -86,6 +120,10 @@ load_functions( function_t* functions, uint64_t const funcs_count, FILE* src ) {
 	return LOAD_OK;
 }
 
+static inline load_err_code_t
+is_header_correct( struct file_header header ) {
+	return  header.tag != 0xDEADDEADDEADDEAD || header.version != 1 ? ERR_INCORRECT_HEADER : LOAD_OK;
+}
 
 load_err_code_t
 load_src_file( vm_t* vm, FILE* src ) {
@@ -95,9 +133,12 @@ load_src_file( vm_t* vm, FILE* src ) {
 		ERR_IN_READING_HEADER
 	);
 
+
 	#ifdef DEBUG
 	print_file_header( header );
 	#endif
+
+	TRY_EXEC( is_header_correct(header) );
 
 	vm_init_str_const_pool( &(vm-> const_str_pool), header.str_pool_size );
 
@@ -119,6 +160,4 @@ load_src_file( vm_t* vm, FILE* src ) {
 
 	return LOAD_OK;
 }
-
-
 
