969163fac10e28f2a9e02b44186d0aa2555a8994
Just a man <vprovalov@yandex.ru>
2017-01-21 04:13:23 +0300

    commands.h and commands.c modified

--- include/commands.h
+++ include/commands.h
@@ -6,6 +6,34 @@
 
 #include <stdio.h>
 
-void interpret( vm_t* const vm, FILE* const errout);
+#define FOR_BYTECODES(DO) \
+	DO(0, nop) \
+	DO(1, push) \
+	DO(2, iadd) \
+	DO(3, dadd) \
+	DO(4, isub) \
+	DO(5, dsub) \
+	DO(6, idiv) \
+	DO(7, ddiv) \
+	DO(8, imul) \
+	DO(9, dmul) \
+	DO(10, mod) \
+	DO(11, branch) \
+	DO(12, branchif) \
+	DO(13, invoke) \
+	DO(14, retvoid) \
+	DO(15, ret) \
+	DO(16, dup) \
+	DO(17, swap) \
+	DO(18, iprint) \
+	DO(19, dprint) \
+	DO(20, sprint) \
+	DO(21, halt) \
+
+#define DEFINE_SIGNATURE( id, name )  \
+enum vm_err_code cmd_##name( vm_t* const vm );
+
+FOR_BYTECODES( DEFINE_SIGNATURE )
 
 #endif
+
--- src/commands.c
+++ src/commands.c
@@ -9,37 +9,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-
-#define FOR_BYTECODES(DO) \
-	DO(nop) \
-	DO(push) \
-	DO(iadd) \
-	DO(dadd) \
-	DO(isub) \
-	DO(dsub) \
-	DO(idiv) \
-	DO(ddiv) \
-	DO(imul) \
-	DO(dmul) \
-	DO(mod) \
-	DO(branch) \
-	DO(branchif) \
-	DO(invoke) \
-	DO(retvoid) \
-	DO(ret) \
-	DO(dup) \
-	DO(swap) \
-	DO(iprint) \
-	DO(dprint) \
-	DO(sprint) \
-	DO(halt) \
-
 #define TRY( action ) \
 do{ int err = (action); if( (err) != OK ) return err; }while(0)
 
 
 #define DEFINE_CMD( cmd_name ) \
-static enum vm_err_code cmd_##cmd_name( vm_t* const vm )
+enum vm_err_code cmd_##cmd_name( vm_t* const vm )
 
 static inline stack_t* get_eval_stack( vm_t const * const vm ) {
 	return vm-> ctx_stack-> cur_ctx-> eval_stack;
@@ -162,40 +137,4 @@ DEFINE_CMD(branchif) {
 DEFINE_CMD(retvoid) {
 	vmctx_free( ctx_stack_pop(vm-> ctx_stack) );
 	return OK;
-}
-
-typedef enum vm_err_code (*cmd_t) (vm_t*);
-
-static inline char get_current_bytecode( vm_t const * const vm ) {
-	vm_context_t const * const cur_ctx = vm-> ctx_stack-> cur_ctx;
-	return cur_ctx-> cur_func-> cmds[ cur_ctx-> instr_ptr ];
-}
-
-static inline void err_handler( enum vm_err_code err, vm_t const * const vm,  FILE* errout ) {
-	if( err == OK ) return;
-
-	fprintf( errout, 
-		"Error: %s ; Instr_ptr: %u ; Instr: %c", 
-		"", 
-		(unsigned int)vm-> ctx_stack-> cur_ctx-> instr_ptr, 
-		get_current_bytecode(vm) 
-		);  
-
-	exit(err);	
-}
-
-
-void interpret( vm_t* const vm , FILE* const errout ) {
-	#define CMDS_PTRS( name ) cmd_##name,
-
-	static const cmd_t cmd_list[] = {
-		FOR_BYTECODES( CMDS_PTRS )
-		0					
-	};
-
-	#undef CMDS_PTRS
-
-	//TODO
-
-	for(;;) { err_handler( cmd_list[ get_current_bytecode(vm) ](vm), vm, errout );	}
 }
